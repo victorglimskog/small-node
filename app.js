c7da283482cfcd146b72333936fd7e7107921704
Victor Glimskog <victor.glimskog@gmail.com>
2017-11-13 14:05:10 +0100

    expressing

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log('Leif GG Persson was here');
+const express = require('express');
+const app = express();
+
+app.use(express.static('www'));
+
+app.listen(3000,() => {
+    console.log('app listening on port 3000')
+});
