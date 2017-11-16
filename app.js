8b6deec1a6d338c123bfe090940f0d5515170e0e
Victor Glimskog <victor.glimskog@gmail.com>
2017-11-16 10:24:11 +0100

    added dirname for static folder address

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static('www'));
+app.use(express.static(__dirname + '/www'));
 
 app.listen(3000,() => {
     console.log('app listening on port 3000')
