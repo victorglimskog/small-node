d7c16965deec76ca33f043c446c9acbcd60ef920
Victor Glimskog <victor.glimskog@gmail.com>
2017-11-27 11:27:48 +0100

    fix

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static(__dirname + '/www'));
+app.use(express.static(__dirname + '/www',{extensions: ['html']}));
 
 app.listen(3000,() => {
     console.log('app listening on port 3000')
